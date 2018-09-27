3dd29a59ef703c93e1f3125c66a6ef39e3920207
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2018-09-27 21:31:19 +0800

    modified the csv file for testing, added the new output with the points column , and implemented Levenshtein distance

--- normalisation.py
+++ normalisation.py
@@ -7,34 +7,35 @@ import numpy as np
 
 # ignore Levenshtein distance for now. It doesn't affect the computation in anyway . Although, it can be used to improve the program
 
-def levenshtein_distance(first_string,second_string):
+def levenshtein_distance(first_string, second_string):
+    first_string = first_string.strip().lower()
+    first_size = len(first_string) + 1
 
-    first_string_char_array = first_string.split()
-    first_size  = len(first_string_char_array) + 1
-    second_string_char_array  = second_string.split()
-    second_size = len(second_string_char_array) + 1
+    second_string = second_string.strip().lower()
+    second_size = len(second_string) + 1
 
     # initializing the distance_array as 0
     distance_array = np.zeros((first_size, second_size))
 
-    for i in range(1,first_size): # valid because first_size = m+1
+    for i in range(1, first_size):  # valid because first_size = m+1
         distance_array[i][0] = i
 
-    for j in range(1,second_size): # valid because second_size = n+1
+    for j in range(1, second_size):  # valid because second_size = n+1
         distance_array[0][j] = j
 
     # since the subsitution cost is a non zero quantity
 
-    for j in range(1 , second_size):
-        for i in range(1 , first_size):
-            if first_string_char_array[i-1] == second_string_char_array[j-1] :
-
-                    substitution_cost = 0
+    for i in range(1, first_size):
+        for j in range(1, second_size):
+            if first_string[i - 1] == second_string[j - 1]:
+                distance_array[i][j] = min(distance_array[i - 1][j] + 1, distance_array[i - 1][j - 1],
+                                           distance_array[i][j - 1] + 1)
             else:
-                    substitution_cost = 1
-                    distance_array[i][j] = min(distance_array[i-1][j]+1 , distance_array[i][j-1]+1 , distance_array[i-1][j-1]+substitution_cost)
+                distance_array[i][j] = min(distance_array[i - 1][j] + 1, distance_array[i - 1][j - 1] + 1,
+                                           distance_array[i][j - 1] + 1)
 
-    return distance_array[first_size-1][second_size-1]
+    return distance_array[first_size - 1][second_size - 1]
+
 
 # The header that have to be set up if the form change
 mapping_headers = {
@@ -46,71 +47,110 @@ mapping_headers = {
     "sports_currently_in": "Sports you are currently in this season (after the latest cut)",
     "core_sport": "What is your core sport?  (Your jersey orders will be given to this TM)",
     "main_jersey": "Main Jersey- Yellow ($17) (for all sports except Track, Road Relay and Trug)",
-    "main_shorts": "Main Shorts (Black) ($15)",
-    "road_relay_singlet": "Road Relay/Track Singlet ($16)",
-    "touch_rugby_singlet": "Touch Rugby Singlet ($16)",
-    "long_socks": "Long Socks (Black) ($12)",
-    "is_handball_or_soccer_keeper_or_a_volleyball_libero": "Are you a Handball/Soccer Keeper or a Volleyball Libero?",
-    "additional_orders": 'Additional orders (indicate jersey type,size and additional quantity)',
     "first_choice": "First Choice",
     "second_choice": "Second Choice",
-    "third_choice": "Third Choice",
-    "touch_rugby_number": "Touch Rugby Number",
-    "main_jersey_handball_soccer": "Main Jersey- Black ($17) (for Handball/Soccer Keepers and Liberos)"
+    "third_choice": "Third Choice"
+
 }
 
+sports = {"soccer", "softball", "rugby", "netball", "dragonboat", "floorball", "tennis", "basketball", "takraw",
+          "swimming", "table tennis", "track", "touch rugby", "road relay", "handball", "squash", "badminton",
+          "water polo","frisbee"}
+data_frame = pd.read_csv("SMC_test_data.csv")
 
-sports = {"soccer", "softball", "rugby", "netball", "dragonboat" , "floorball", "tennis" , "basketball" , "takraw", "swimming", "table tennis" , "track" , "touch rugby" , "road relay"  , "handball", "squash" , "badminton" , "water polo" }
-data_frame= pd.read_csv("SMC_test_data.csv" )
 
 def yes_no(string):
     answer = "yes"
-    if (difflib.SequenceMatcher(None , answer , string).ratio() >0.7):
+    if (difflib.SequenceMatcher(None, answer, string).ratio() > 0.7):
         return 1
     else:
         return 0
 
 
 final_score = []
+is_captain_score_list = []
+all_sports_enrolled_in_score_list = []
+all_sports_represented_IVP_SUNIG_list = []
 
- # calculates the final points .. takes into consideration .. the possible
- # since a player is a captain of atmost 1 team .. consider yes / no
+
+def calculate_points_subroutine(iter, the_column, the_set):
+    score = 0
+    the_cell_partition = the_column[iter].split(",")
+
+    # keep NONE as a option for
+    if "none" in the_cell_partition:
+        return score
+
+    for sport in the_cell_partition:
+        sport = sport.strip().lower()
+        for s in the_set:
+            if levenshtein_distance(sport, s) <= 3:
+                score += 1
+                break
+
+    return score
+
+
+# calculates the final points .. takes into consideration .. the possible
+# since a player is a captain of atmost 1 team .. consider yes / no
 def calculate_points():
-    data_frame= pd.read_csv("SMC_test_data.csv")
+    data_frame = pd.read_csv("SMC_test_data_2.csv")
     is_captain_column = data_frame[str(mapping_headers["is_captain"])]
+    is_captain_column = is_captain_column.str.lower()
+    is_captain_column = is_captain_column.str.strip()
+
+    # stripping all the white spaces and the converting into lower case in the sports column
     sports_column = data_frame[str(mapping_headers["sports_currently_in"])]
+    sports_column = sports_column.str.lower()
+    sports_column = sports_column.str.strip()
+
+    # stripping all the white spaces and the converting into lower case in the sports_represented  column
     representing_sports_column = data_frame[str(mapping_headers["representing_sports_ivp"])]
+    representing_sports_column = representing_sports_column.str.lower()
+    representing_sports_column = representing_sports_column.str.strip()
 
-
-    for i in range(len(is_captain_column)):
+    for iteration in range(len(is_captain_column)):
         score = 0
-        score += yes_no(is_captain_column[i].lower())
+        score += yes_no(is_captain_column[iteration])
+        is_captain_score_list.insert(iteration, score)  # enter 0 or 1 in the is_captain_score Column
 
-        all_sports_currently_involved_in = sports_column[i].split(",")
-        # counting points for all the sports currently represented
+        # counting points for all the sports currently involved in
+        all_sports_currently_involved_in_score = calculate_points_subroutine(iteration, sports_column, sports)
+        # adding the score for number of sports enrolled in the
+        all_sports_enrolled_in_score_list.insert(iteration, all_sports_currently_involved_in_score)
+        score += all_sports_currently_involved_in_score
 
-        for sport in all_sports_currently_involved_in:
-            sport = sport.strip().lower()
+        # calc for score on the basis of sports represented in IVP and SUNIG
+        all_sports_represented_score = calculate_points_subroutine(iteration, representing_sports_column, sports)
+        # adding the score of representing the sport at IVP/SUNIG in the list
+        all_sports_represented_IVP_SUNIG_list.insert(iteration, all_sports_represented_score)
+        score += all_sports_represented_score
 
-
-            for s in sports:
-                if difflib.SequenceMatcher(None, s, sport).ratio() >= 0.7:
-                    score+= 1
-
-        all_sports_represented = representing_sports_column[i].split(",")
-
-        for sports_represented in all_sports_represented:
-            sports_represented = sports_represented.strip().lower()
-
-            for s in sports: # the set
-                if difflib.SequenceMatcher(None, s , sports_represented).ratio() >= 0.7 :
-                    score+= 1
-
-        final_score.insert(i , score)
+        final_score.insert(iteration, score)
 
 
 calculate_points()
 data_frame["total_points"] = final_score
 
-#uncomment the line below to test
-#print(data_frame["total_points"])
+
+print(" FINAL POINTS")
+print(final_score)
+print()
+print("CAPTAIN POINTS")
+print(is_captain_score_list)
+print()
+print(" SPORTS ENROLLED IN POINTS : ")
+print(all_sports_enrolled_in_score_list)
+print()
+print(" ALL SPORTS REPRESENTED : ")
+print(all_sports_represented_IVP_SUNIG_list)
+print()
+
+
+data_frame["captain_points"] = is_captain_score_list
+data_frame["sports_enrolled_in_points"] = all_sports_enrolled_in_score_list
+data_frame["sports_represented_points"] = all_sports_represented_IVP_SUNIG_list
+
+file_name = str(input("Enter the output file name :"))
+
+data_frame.to_csv(file_name, sep=',' , encoding='utf-8')
