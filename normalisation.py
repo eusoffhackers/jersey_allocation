72da297eaa6fd5eeba7c21a793f747a010b2ef2e
Eeshan Jaiswal <esjaiswal1@sheffield.ac.uk>
2018-09-30 18:06:24 +0800

    Update normalisation.py

--- normalisation.py
+++ normalisation.py
@@ -94,7 +94,7 @@ def calculate_points_subroutine(iter, the_column, the_set):
 # calculates the final points .. takes into consideration .. the possible
 # since a player is a captain of atmost 1 team .. consider yes / no
 def calculate_points():
-    data_frame = pd.read_csv("SMC_test_data_2.csv")
+    data_frame = pd.read_csv("SMC_test_data.csv")
     is_captain_column = data_frame[str(mapping_headers["is_captain"])]
     is_captain_column = is_captain_column.str.lower()
     is_captain_column = is_captain_column.str.strip()
